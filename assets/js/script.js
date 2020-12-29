d146bbec97f367c7d8cca11973c04540a830b8be
dvicjon <devinvjones@gmail.com>
2020-12-29 12:21:32 -0500

    added edit and delete functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0; 
+var pageContentEl = document.querySelector("#page-content");
 
 var taskFormHandler = function (event) {
     event.preventDefault(); 
@@ -86,4 +87,44 @@ var createTaskActions = function(taskId) {
     return actionContainerEl; 
 };
 
+var taskButtonHandler = function(event) {
+    //get target element from event 
+    var targetEl = event.target; 
+
+    //if edit button was clicked 
+    if (targetEl.matches(".edit-btn")) {
+        var taskId = targetEl.getAttribute("data-task-id");
+        editTask(taskId);
+    }
+
+    //if delete button was clicked 
+    else if (targetEl.matches(".delete-btn")) {
+        //get element's task id
+        var taskId = targetEl.getAttribute("data-task-id");
+        deleteTask(taskId); 
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove(); 
+};
+
+var editTask = function(taskId) {
+    console.log("editing task #" + taskId);
+
+    //get task list element 
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    //get content from task name and type 
+    var taskName = taskSelected.querySelector("h3.task-name").textContent; 
+    console.log(taskName); 
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+    console.log(taskType); 
+
+    document.querySelector("input[name='task-name']").value = taskName;
+    document.querySelector("select[name='task-type']").value = taskType;
+    document.querySelector("#save-task").textContent = "Save Task";
+}
+
 formEl.addEventListener("submit", taskFormHandler);
+pageContentEl.addEventListener("click", taskButtonHandler);
